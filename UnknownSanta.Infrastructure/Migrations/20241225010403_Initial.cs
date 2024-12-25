14242b0ccb7f9a4806c2ae21f78e75e8afd4e7e6
Egor <123485933+QualityKillom@users.noreply.github.com>
2024-12-25 04:04:14 +0300

    я уже ничего не хочу понимать

---
+++ UnknownSanta.Infrastructure/Migrations/20241225010403_Initial.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnknownSanta.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Game_Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Currency = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    ChatType = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    GameState = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Game_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SendMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Telegram_Id = table.Column<long>(type: "INTEGER", nullable: false),
+                    SentAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SendMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TagUserName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Participle = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Game_Id = table.Column<long>(type: "INTEGER", nullable: false),
+                    Telegram_Id = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Games_Game_Id",
+                        column: x => x.Game_Id,
+                        principalTable: "Games",
+                        principalColumn: "Game_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Game_Id",
+                table: "Users",
+                column: "Game_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SendMessages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
--- UnknownSanta.Infrastructure/obj/Debug/net9.0/UnknownSanta.Infrastructure.AssemblyInfo.cs
+++ UnknownSanta.Infrastructure/obj/Debug/net9.0/UnknownSanta.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnknownSanta.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e235146c531b957f5522260c7e7a7ba7cd7795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f58f9d5ce968bbf5e5b07bcdb22a54104f2008")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnknownSanta.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnknownSanta.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
